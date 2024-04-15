74c6f142a930ad303f9f28b652fe2e1c0cf01201
norb_ <127006485+devnorb@users.noreply.github.com>
2024-04-15 16:43:48 -0700

    Update betterbypasserfix.lua

--- betterbypasserfix.lua
+++ betterbypasserfix.lua
@@ -1,2 +1,8 @@
+local Metatable = getrawmetatable(game:GetService("StarterGui"))
+setreadonly(Metatable, false)
+local old;
+old = hookfunction(setreadonly, function() return true end)
+-- or else error
 debug.getfenv = getfenv
+-- bro no way the devs did debug.getfenv lmfao
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/BetterBypasser/loader.lua",true))()
